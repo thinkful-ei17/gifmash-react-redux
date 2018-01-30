085e2be9c3f2ab4bff58c366000093089ef88f1f
Sam Gould <sjg326@gmail.com>
2018-01-30 11:24:44 -0800

    basic react framework for comparing two gifs

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GifContainer from './gif-container'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GifContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif-container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Gif from './gif';
+
+export default function GifContainer() {
+    return (
+        <div>
+            <Gif />
+            <Gif />
+        </div>
+    )
+}
